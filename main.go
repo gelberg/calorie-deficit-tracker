6e8ced879d8e310294d0111ff267e6d0fde5a49e
Aleksandr Gelberg <54027156+gelberg@users.noreply.github.com>
2024-03-14 13:10:58 +0400

    Started auth implementation according to twitteroob example (https://github.com/gomodule/oauth1/blob/master/examples/twitteroob/main.go).

--- main.go
+++ main.go
@@ -4,57 +4,52 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"net/http"
 	"net/url"
+	"os"
 
 	"github.com/gomodule/oauth1/oauth"
 )
 
+var oauthClient = oauth.Client{
+	TemporaryCredentialRequestURI: "https://www.fatsecret.com/oauth/request_token",
+	ResourceOwnerAuthorizationURI: "https://www.fatsecret.com/oauth/authorize",
+	TokenRequestURI:               "https://www.fatsecret.com/oauth/access_token",
+}
+
 func main() {
-	client := oauth.Client{}
-	client.Credentials.Token = "***REMOVED***"
-	client.Credentials.Secret = "***REMOVED***"
-	client.SignatureMethod = oauth.HMACSHA1
+	oauthClient.Credentials.Token = "***REMOVED***"
+	oauthClient.Credentials.Secret = "***REMOVED***"
+	oauthClient.SignatureMethod = oauth.HMACSHA1
 
 	values := url.Values{
-		"method": {"food_entries.get_month"},
-		"format": {"json"},
-		"date":   {"19782"},
+		// "oauth_callback": {"oob"},
 	}
-	u, err := url.Parse("https://platform.fatsecret.com/rest/server.api")
+	oauthClient.SignForm(nil, "POST", oauthClient.TemporaryCredentialRequestURI, values, "oob")
+	tempCred, err := oauthClient.RequestTemporaryCredentials(nil, "oob", values)
+	if err != nil {
+		log.Fatal("RequestTemporaryCredentials:", err)
+	}
+
+	u := oauthClient.AuthorizationURL(tempCred, nil)
+
+	fmt.Printf("1. Go to %s\n2. Authorize the application\n3. Enter verification code:\n", u)
+
+	var code string
+	fmt.Scanln(&code)
+
+	tokenCred, _, err := oauthClient.RequestToken(nil, tempCred, code)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	creds := oauth.Credentials{Token: "***REMOVED***", Secret: "***REMOVED***"}
-
-	fmt.Println("values: ", values)
-	client.SignForm(&creds, "food_entries.get_month", u.String(), values)
-	fmt.Println("values: ", values)
-
-	fmt.Println("Header: ", client.Header)
-	fmt.Println("Credentials: ", client.Credentials)
-	header := http.Header{}
-	err = client.SetAuthorizationHeader(header, &creds, "food_entries.get_month", u, values)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	client.Header = header
-	fmt.Println("Header: ", client.Header)
-	fmt.Println("Credentials: ", client.Credentials)
-
-	resp, err := client.Get(nil, &creds, u.String(), values)
+	oauthClient.SignForm(nil, "GET", "https://platform.fatsecret.com/rest/server.api", values, "")
+	resp, err := oauthClient.Get(nil, tokenCred,
+		"https://platform.fatsecret.com/rest/server.api?method=food_entries.get_month&format=json&date=19782", values)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-		return
+	if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+		log.Fatal(err)
 	}
-	fmt.Println(string(body))
 }
